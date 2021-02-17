a9bc65bf36ecd39fc863404bc515679638aa6619
Mykola Shostak <mykolashostak@gmail.com>
2021-02-17 12:12:19 +0200

    created a new array and applied a loop to it

--- HW-1/show.js
+++ HW-1/show.js
@@ -1,12 +1,17 @@
 //Відображення в Html
-document.getElementById("max_price").innerHTML = `Максимальна ціна: ${maxNumber} грн.`;
-document.getElementById("min_price").innerHTML = `Мінімальна ціна: ${minNumber} грн.`;
-document.getElementById("total_price").innerHTML = `Сума всіх товарів: ${sumPriceAll}`;
-document.getElementById("total_price_floor").innerHTML = `Сума товарів з округленням в меншу сторону: ${floorPriceAll} грн.`;
-document.getElementById("total_price_hundredth").innerHTML = `Сума товарів з округленням до сотень: ${sumAllNumber} грн.`;
-document.getElementById("even_or_odd").innerHTML = `Сума товарів є ${result? 'непарним' : 'парним'} числом`;
-document.getElementById("client_change").innerHTML = `Решта клієнта з 500 грн.: ${changeClient} грн.`;
-document.getElementById("average").innerHTML = `Среднє значення: ${averageAllNumder} грн.`;
-document.getElementById("sale").innerHTML = `Знижка: ${saleWithHundredth} грн.`;
-document.getElementById("to_pay").innerHTML = `До сплати: ${toPayHundredth} грн.`;
-document.getElementById("no_pat").innerHTML = `Чистий прибуток: ${noPatHundredth} грн.`;
+const allCalculations = [`Максимальна ціна: ${maxNumber} грн.`,
+    `Мінімальна ціна: ${minNumber} грн.`, `Сума всіх товарів: ${sumPriceAll}`,
+    `Сума товарів з округленням в меншу сторону: ${floorPriceAll} грн.`,
+    `Сума товарів з округленням до сотень: ${sumAllNumber} грн.`,
+    `Сума товарів є ${result? 'непарним' : 'парним'} числом`,
+    `Решта клієнта з 500 грн.: ${restClient} грн.`,
+    `Среднє значення: ${averageAllNumder} грн.`,
+    `Знижка: ${saleWithHundredth} грн.`,
+    `До сплати: ${toPayHundredth} грн.`,
+    `Чистий прибуток: ${noPatHundredth} грн.`
+]
+allCalculations.forEach(el => console.log(el))
+const elements = Array.from(document.querySelectorAll('.сalculation'));
+console.log(elements);
+let i = 0;
+elements.forEach(el => el.innerHTML = allCalculations[i++]);

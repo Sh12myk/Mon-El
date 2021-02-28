243bcbf16829aae08dd210f51bb87b11d806c698
Mykola Shostak <mykolashostak@gmail.com>
2021-02-28 03:56:19 +0200

    Update app.js

--- HW-2/js/app.js
+++ HW-2/js/app.js
@@ -3,52 +3,74 @@
     const btnNext = document.querySelector('.btn-submit')
     const formAll = document.querySelectorAll('form')
     const btnBack = document.querySelector('.btn-back')
+    const form = document.getElementById('evenChoice');
+    const resultLabel = document.getElementById('result');
 
 
     let activeStep = 0
     btnNext.addEventListener('click', setStatus)
     btnBack.addEventListener('click', setBackStatus)
 
-    btnNext.addEventListener('click', userNumber);
 
+    const style = 'background: white; border: 1px solid gold; color: black; font-size: 30px; margin: 20px; padding: 20px;'
 
-    function userNumber() {
+    function checkTheEnteredUserNumber() {
+        btnNext.addEventListener('click', checkTheEnteredUserNumber);
+        btnNext.removeEventListener('click', checkTheEnteredUserNumber);
         const startNumber = Math.min(firstNumber.value, secondNumber.value);
         const endNumber = Math.max(firstNumber.value, secondNumber.value);
-        console.log(`You want to know the sum of all numbers from  ${ startNumber } to ${endNumber}`)
-        btnNext.removeEventListener('click', userNumber);
+        console.log(`%cYou want to know the sum of all numbers from  ${ startNumber } to ${endNumber}`, style)
+        btnNext.addEventListener('click', mathSum);
+        return [startNumber, endNumber]
 
     }
-    const form = document.getElementById('evenChoice');
-    const resultLabel = document.getElementById('result');
+    btnNext.removeEventListener('click', checkTheEnteredUserNumber);
 
 
-    form.onsubmit = (e) => {
-        e.preventDefault();
-        const startNumber = Math.min(firstNumber.value, secondNumber.value);
-        const endNumber = Math.max(firstNumber.value, secondNumber.value);
+
+    function mathSum() {
+        btnNext.removeEventListener('click', checkTheEnteredUserNumber);
+        const ourNumbers = checkTheEnteredUserNumber()
         const skipEven = document.querySelector("input[name=choice]:checked").value;
-        let result = 0;
+        const startNumber = ourNumbers[0]
+        const endNumber = ourNumbers[1]
+        let result = 0
         for (let loopNumbers = startNumber; loopNumbers <= endNumber; loopNumbers++) {
             {
-                if (skipEven === `true` && loopNumbers % 2 === 0) {
+                if (skipEven === `true` && !(loopNumbers % 2)) {
                     continue;
                 }
                 result += loopNumbers;
 
             }
         }
-        console.log(`Want to skip even numbers? ${skipEven}`);
-        console.log(`Youre result: ${result}`)
-        resultLabel.innerHTML = `result: ${result}`;
+        return [result, skipEven]
     }
+
+
+
+    evenChoice.onsubmit = (e) => {
+        e.preventDefault();
+        const arrMathSum = mathSum()
+        const result = arrMathSum[0]
+        const skipEven = arrMathSum[1]
+        console.log(`%cWant to skip even numbers? ${skipEven}`, style);
+        console.log(`%cYoure result: ${result}`, style)
+        resultLabel.innerHTML = `Result: ${result}`;
+        btnNext.addEventListener('click', addNewHistoryEntry);
+        btnNext.removeEventListener('click', checkTheEnteredUserNumber);
+        btnNext.addEventListener('click', fillTable);
+    }
+
+
 
     function setStatus(event) {
         event.preventDefault()
         if (activeStep < stepAll.length) {
             stepAll[activeStep].classList.add('done')
             formAll[activeStep].classList.toggle('hidden')
-        }
+        } else
+            return
         if (activeStep < stepAll.length - 1) {
             stepAll[activeStep + 1].classList.add('active')
             formAll[activeStep + 1].classList.toggle('hidden')
@@ -62,9 +84,40 @@
             formAll[activeStep].classList.toggle('hidden')
         } else
             return
-        activeStep--
+
+            activeStep--
     }
-    let history = []
 
 
+    function addNewHistoryEntry() {
+        btnNext.addEventListener('click', fillTable);
+        btnNext.removeEventListener('click', checkTheEnteredUserNumber);
+        const history = []
+        const arrMathSum = mathSum()
+        const result = arrMathSum[0]
+        const ourNumbers = checkTheEnteredUserNumber()
+        const startNumber = ourNumbers[0]
+        const endNumber = ourNumbers[1]
+        const newEntry = [startNumber, endNumber, result]
+        history.push(newEntry)
+
+        return history
+    }
+
+
+    const table = document.querySelector('table')
+
+    function fillTable() {
+        const myHistory = addNewHistoryEntry()
+        for (let i = 0; i < myHistory.length; i++) {
+            const tr = document.createElement('tr')
+            for (let j = 0; j < myHistory[i]; j++) {
+                td.innerHTML = myHistory[i][j]
+                const td = document.createElement('td')
+                tr.appendChild(td)
+            }
+            table.appendChild(tr)
+        }
+
+    }
 }

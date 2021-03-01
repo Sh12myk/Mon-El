55068b291e924fda4ac1a43b0cb1b433c989efaa
Mykola Shostak <mykolashostak@gmail.com>
2021-03-01 23:49:15 +0200

    Create Random.js

---
+++ HW-3/js/Random.js
@@ -0,0 +1,17 @@
+const btnResult = document.querySelector('.btn-result')
+const resultLabel = document.getElementById('result');
+
+function randomInteger() {
+    const userFirstNumber = document.getElementById('userFirstNumber').value;
+    const userSecondNumber = document.getElementById('userSecondNumber').value;
+    const rand = +userFirstNumber + Math.random() * (+userSecondNumber + 1 - +userFirstNumber);
+    return Math.floor(rand);
+}
+
+function showResult() {
+    const randomNumber = randomInteger()
+    document.getElementById("result").innerHTML = randomNumber
+}
+
+btnResult.addEventListener('click', showResult)
+btnResult.addEventListener('click', randomInteger)

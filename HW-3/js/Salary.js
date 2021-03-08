e3762aac910f46e90d93576022b2a4cb4736eb60
Mykola Shostak <mykolashostak@gmail.com>
2021-03-08 09:08:12 +0200

    Update Salary.js

--- HW-3/js/Salary.js
+++ HW-3/js/Salary.js
@@ -3,8 +3,8 @@ const resultLabel = document.getElementById('result');
 
 function calculateMySalary() {
     const userSalary = document.getElementById('userSalary').value;
-    if (userSalary % 1 !== 0) {
-        document.getElementById("userSalary").innerHTML = `Error, enter correct numbers`
+    if ((userSalary % 1) || (Math.sign(userSalary) === -1)) {
+        document.getElementById("result").innerHTML = `Error, enter correct numbers`
     } else {
         let salaryWithoutTax = userSalary - userSalary * 0.195;
         document.getElementById("result").innerHTML = `Salary after taxes = ${salaryWithoutTax}`

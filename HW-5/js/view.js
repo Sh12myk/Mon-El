5a9e0546c3faa8e1e677278faf596b96b7c08a41
Shostak Mykola <mykolashostak@gmail.com>
2021-03-15 10:58:22 +0200

    Create view.js

---
+++ HW-5/js/view.js
@@ -0,0 +1,13 @@
+const arrResult = [`Функція №1 : ${getRandomArray(1, 9, 35) }`,
+ `Функція №2 : ${findMode(arn)}`,
+ `Функція №3 : ${result}`,
+  `Функція №4 : ${median}`,
+  `Функція №5 : ${oddNumbers}`,
+  `Функція №6 : ${countPositive}`,
+  `Функція №7 : ${dividedByFive}`]
+  
+  arrResult.forEach(el => console.log(el))
+  
+const elements = Array.from(document.querySelectorAll('.result'));
+let i = 0;
+elements.forEach(el => el.innerHTML = arrResult[i++]);

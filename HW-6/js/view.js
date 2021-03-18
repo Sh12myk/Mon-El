1c329f3bc18e4d6c9677ff2722dab72f386cc23f
Shostak Mykola <mykolashostak@gmail.com>
2021-03-18 19:33:29 +0200

    Create view.js

---
+++ HW-6/js/view.js
@@ -0,0 +1,8 @@
+const arrResult = [getSubjects(students[0]) ,
+ getAverageMark(students[2]),
+ getStudentInfo(students[0]),
+  getStudentsNames(students),
+  getBestStudent(students),
+  calculateWordLetters('циклопентанпергіндрофенантрен')]
+  
+  arrResult.forEach(el => console.log(el))

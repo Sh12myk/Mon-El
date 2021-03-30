bb5467f2b47dc5f20c72dbac04aaf97c0a781b60
Shostak Mykola <mykolashostak@gmail.com>
2021-03-31 02:45:25 +0300

    Create main.js

---
+++ HW-10/js/main.js
@@ -0,0 +1,22 @@
+
+function generateBlocks (){
+  for (let i = 0; i < 25;i++ ) {
+    const div = document.createElement('div'); 
+    div.setAttribute("id" , "new" + i);
+    div.setAttribute("class" , "block");
+    document.body.appendChild(div);
+  }
+  document.querySelectorAll('.block').forEach(e => e.style.backgroundColor = getRandomColor());
+}
+function getRandomColor(){
+  const r = () => {return Math.floor(Math.random()*10)};
+  return "#" + r() + r()+ r() + r() + r()+ r();
+}
+function generateBlocksInterval(){
+  setInterval(() => {
+    document.querySelectorAll('.block').forEach(e => e.style.backgroundColor = getRandomColor());
+}, 1000);
+}
+generateBlocks()
+getRandomColor()
+generateBlocksInterval()

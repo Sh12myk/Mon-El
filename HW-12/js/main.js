1b2782e0804b091267713260bf482d05c5ec763c
Shostak Mykola <mykolashostak@gmail.com>
2021-04-15 17:03:29 +0300

    Create main.js

---
+++ HW-12/js/main.js
@@ -0,0 +1,20 @@
+async function getKorean(length) {
+    let res = '';
+    for (let i = 0; i < length; i++) {
+
+
+        const promise = new Promise((resolve) => {
+            setTimeout(() => {
+                let date = Date.now().toString();
+                date = +date.split('').splice(-5).join('')
+                const chr = String.fromCharCode(date)
+                resolve(chr)
+            }, 50);
+
+        });
+        await promise.then((chr) => res += chr)
+    }
+    console.log(res);
+}
+
+getKorean(5)

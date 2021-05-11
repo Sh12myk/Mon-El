b6aa175a3327dfb52faa21836edbe5f6abb483b0
Shostak Mykola <mykolashostak@gmail.com>
2021-05-11 19:23:09 +0300

    Create index.js

---
+++ HW-15/src/index.js
@@ -0,0 +1,31 @@
+const moduleHW1 = require('../../HW-1/index')
+const moduleHW2 = require('../../HW-2/js/app')
+const moduleHW3 = require('../../HW-3/js/Random')
+const moduleHW4 = require('../../HW-4/js/main')
+const moduleHW5 = require('../../HW-5/js/view')
+const moduleHW6 = require('../../HW-6/js/view')
+const moduleHW7 = require('../../HW-7/js/main')
+const moduleHW8 = require('../../HW-8/js/index')
+const moduleHW10 = require('../../HW-10/js/main')
+const moduleHW11 = require('../../HW-11/js/main')
+const moduleHW12 = require('../../HW-12/js/main')
+const moduleHW13 = require('../../HW-13/js/main')
+const moduleHW14 = require('../../HW-14/js/main')
+
+const allImportFunction = [
+    `HW1:${moduleHW1}`
+    `HW2:${(moduleHW2)}`
+    `HW3:${moduleHW3}`
+    `HW4:${moduleHW4}`
+    `HW5:${moduleHW5}`
+    `HW6:${moduleHW6}`
+    `HW7:${moduleHW7}`
+    `HW8:${moduleHW8}`
+    `HW9:${moduleHW10}`
+    `HW10:${moduleHW11}`
+    `HW11:${moduleHW12}`
+    `HW12:${moduleHW13}`
+    `HW13:${moduleHW14}`
+    `HW14:${moduleHW15}`
+]
+allImportFunction.forEach(el => console.log((el)))()

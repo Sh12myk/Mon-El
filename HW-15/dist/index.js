39c64d52334fb21bdbf22cf7c657258beec37956
Shostak Mykola <mykolashostak@gmail.com>
2021-05-11 19:32:55 +0300

    Create index.js

---
+++ HW-15/dist/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var _templateObject = _taggedTemplateLiteral(['HW14:', ''], ['HW14:', '']),
+    _templateObject2 = _taggedTemplateLiteral(['HW13:', ''], ['HW13:', '']),
+    _templateObject3 = _taggedTemplateLiteral(['HW12:', ''], ['HW12:', '']),
+    _templateObject4 = _taggedTemplateLiteral(['HW11:', ''], ['HW11:', '']),
+    _templateObject5 = _taggedTemplateLiteral(['HW10:', ''], ['HW10:', '']),
+    _templateObject6 = _taggedTemplateLiteral(['HW9:', ''], ['HW9:', '']),
+    _templateObject7 = _taggedTemplateLiteral(['HW8:', ''], ['HW8:', '']),
+    _templateObject8 = _taggedTemplateLiteral(['HW7:', ''], ['HW7:', '']),
+    _templateObject9 = _taggedTemplateLiteral(['HW6:', ''], ['HW6:', '']),
+    _templateObject10 = _taggedTemplateLiteral(['HW5:', ''], ['HW5:', '']),
+    _templateObject11 = _taggedTemplateLiteral(['HW4:', ''], ['HW4:', '']),
+    _templateObject12 = _taggedTemplateLiteral(['HW3:', ''], ['HW3:', '']),
+    _templateObject13 = _taggedTemplateLiteral(['HW2:', ''], ['HW2:', '']);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+var moduleHW1 = require('../../HW-1/index');
+var moduleHW2 = require('../../HW-2/js/app');
+var moduleHW3 = require('../../HW-3/js/Random');
+var moduleHW4 = require('../../HW-4/js/main');
+var moduleHW5 = require('../../HW-5/js/view');
+var moduleHW6 = require('../../HW-6/js/view');
+var moduleHW7 = require('../../HW-7/js/main');
+var moduleHW8 = require('../../HW-8/js/index');
+var moduleHW10 = require('../../HW-10/js/main');
+var moduleHW11 = require('../../HW-11/js/main');
+var moduleHW12 = require('../../HW-12/js/main');
+var moduleHW13 = require('../../HW-13/js/main');
+var moduleHW14 = require('../../HW-14/js/main');
+
+var allImportFunction = [('HW1:' + moduleHW1)(_templateObject13, moduleHW2)(_templateObject12, moduleHW3)(_templateObject11, moduleHW4)(_templateObject10, moduleHW5)(_templateObject9, moduleHW6)(_templateObject8, moduleHW7)(_templateObject7, moduleHW8)(_templateObject6, moduleHW10)(_templateObject5, moduleHW11)(_templateObject4, moduleHW12)(_templateObject3, moduleHW13)(_templateObject2, moduleHW14)(_templateObject, moduleHW15)];
+allImportFunction.forEach(function (el) {
+    return console.log(el);
+})();
